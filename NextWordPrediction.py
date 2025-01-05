35e80ee7aeb2e7d413861d38fea70fa37731d29a
Dakshayini <137468494+dakshayini28@users.noreply.github.com>
2025-01-05 08:38:57 +0530

    Update NextWordPrediction.py

--- NextWordPrediction.py
+++ NextWordPrediction.py
@@ -20,18 +20,14 @@ tokenizer = load_tokenizer()
 
 max_sequence_len = 18 
 
-def generate_text(seed_text, next_words):
-    for _ in range(next_words):
-        token_list = tokenizer.texts_to_sequences([seed_text])[0]
-        token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
-        predicted = np.argmax(model.predict(token_list), axis=-1)
-        output_word = ""
-        for word, index in tokenizer.word_index.items():
-            if index == predicted:
-                output_word = word
-                break
-        seed_text += " " + output_word
-    return seed_text
+def generate_text(text, num_words):
+    reverse_word_index = {index: word for word, index in tokenizer.word_index.items()}
+    for _ in range(num_words):
+        token_text = tokenizer.texts_to_sequences([text])[0]
+        padded_token_text = pad_sequences([token_text], maxlen=max_sequence_len-1, padding='pre')
+        pos = np.argmax(model.predict(padded_token_text), axis=-1)[0]
+        text += " " + reverse_word_index.get(pos, "")
+    return text
 
 def display_predictions():
     st.title("Next Word Prediction")
